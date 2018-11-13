3c892b422eb02da48e343f9a02dc514dc4bc173f
Gab <gulacsi.gab@gmail.com>
2018-11-13 11:34:20 +0100

    Add project files.

---
+++ WeatherFC/ViewModels/MainWindowVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherFC.ViewModels
+{
+    class MainWindowVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        static MainWindowVM entity;
+        public static MainWindowVM Get()
+        {
+            if (entity == null) entity = new MainWindowVM();
+            return entity;
+        }
+    }
+}
